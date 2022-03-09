dfd6a9f4c8befb232457e97e37aa3acf25d21013
hokuson <git config --global user.email>
2022-03-09 22:42:12 +0900

    comit

--- js/script.js
+++ js/script.js
@@ -6,15 +6,36 @@ $(function () {
   });
 });
 
-$(document).ready(function() {
-  $('.js-replace-no-image').error(function() {
-      $(this).attr({
-          src: "img/Noimage.png",
-          alt: 'no image'
-      });
+$(document).ready(function () {
+  $(".js-replace-no-image").error(function () {
+    $(this).attr({
+      src: "img/Noimage.png",
+      alt: "no image",
+    });
   });
 });
 
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 2,
+  spaceBetween: 56,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+
 $(".openbtn6").click(function () {
-  $(this).toggleClass('active');
+  $(this).toggleClass("active");
 });
