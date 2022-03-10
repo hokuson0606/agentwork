4235f383c6b8ae78a210171fff900164fce4b7fa
hokuson <git config --global user.email>
2022-03-10 19:04:03 +0900

    push

--- js/script.js
+++ js/script.js
@@ -39,3 +39,24 @@ const swiper = new Swiper(".swiper", {
 $(".openbtn6").click(function () {
   $(this).toggleClass("active");
 });
+
+const swiper2 = new Swiper(".swiper2", {
+  slidesPerView: 3,
+  spaceBetween: 16,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
