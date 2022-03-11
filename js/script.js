0edaf3a5767e886e0119e92ef2f5363ee11ec9a6
hokuson <git config --global user.email>
2022-03-11 17:52:35 +0900

    comit

--- js/script.js
+++ js/script.js
@@ -41,7 +41,7 @@ $(".openbtn6").click(function () {
 });
 
 const swiper2 = new Swiper(".swiper2", {
-  slidesPerView: 3,
+  slidesPerView: 4.5,
   spaceBetween: 16,
 
   // If we need pagination
@@ -58,5 +58,22 @@ const swiper2 = new Swiper(".swiper2", {
   // And if we need scrollbar
   scrollbar: {
     el: ".swiper-scrollbar",
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
   },
 });
